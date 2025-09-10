95839f51af2e48e99782170b4197e129bf5196da
umaritics <umar.irpk@gmail.com>
2025-09-11 00:16:23 +0500

    Created the complete home page

--- app/src/main/java/com/example/socially/login2.kt
+++ app/src/main/java/com/example/socially/login2.kt
@@ -14,14 +14,18 @@ class login2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login2)
-        var signup = findViewById<TextView>(R.id.signup)
+        var signupText = findViewById<TextView>(R.id.signup)
+        var login = findViewById<TextView>(R.id.login_button)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        signup.setOnClickListener {
+        signupText.setOnClickListener {
             startActivity(android.content.Intent(this, signup::class.java))
+        }
+        login.setOnClickListener {
+            startActivity(android.content.Intent(this, main_page::class.java))
         }
     }
 }
