28eb28e074247d86ebec36ed95c34cad64bf40ef
umaritics <umar.irpk@gmail.com>
2025-09-11 01:07:16 +0500

    Created the chats page

--- app/src/main/java/com/example/socially/main_page.kt
+++ app/src/main/java/com/example/socially/main_page.kt
@@ -1,20 +1,28 @@
 package com.example.socially
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class main_page : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_page)
+        var chat_button = findViewById<ImageView>(R.id.chats_button)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        chat_button.setOnClickListener {
+            startActivity(android.content.Intent(this, chats::class.java))
+        }
     }
+
 }
