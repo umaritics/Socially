95453c6ea6a76cc3b398c4dbe58ba7681411636f
umaritics <umar.irpk@gmail.com>
2025-09-13 03:36:54 +0500

    Created internshala chat page

--- app/src/main/java/com/example/socially/MainActivity.kt
+++ app/src/main/java/com/example/socially/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_internshala_chat)
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
